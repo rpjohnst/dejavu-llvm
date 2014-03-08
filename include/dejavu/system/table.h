ab61021990272a5e5fdf228383aa05eed5a775ba
Russell Johnston <rpjohnst@gmail.com>
2014-03-07 23:21:21 -0700

    Added remove function and size/capacity tracking to tables

--- include/dejavu/system/table.h
+++ include/dejavu/system/table.h
@@ -18,9 +18,13 @@ template <
 	class key, class value, class hash = hash<key>, class equal = equal<key>
 >
 class table {
-	struct node;
-
 public:
+	struct node {
+		key k;
+		value v;
+		node *next;
+	};
+
 	table(size_t s = 1) { resize(s); }
 	~table() { delete[] contents; }
 
@@ -30,14 +34,15 @@ public:
 		else return insert(k);
 	}
 
+	// don't insert an already-existing node
 	value &insert(const key &k) {
 		node *main = main_position(k);
 
 		if (!is_empty(main)) {
 			node *free = find_free();
 			if (free == end()) {
-				resize(size * 2);
-				return (*this)[k];
+				resize(length * 2);
+				return insert(k);
 			}
 
 			node *other = main_position(main->k);
@@ -57,7 +62,9 @@ public:
 			main->next = nullptr;
 		}
 
+		count++;
 		main->k = k;
+		main->v = value();
 		return main->v;
 	}
 
@@ -77,34 +84,60 @@ public:
 		return end();
 	}
 
-	node *end() {
-		return contents + size;
+	void remove(const key &k) {
+		node *n = find(k);
+		if (n == end())
+			return;
+
+		if (n->next) {
+			node *next = n->next;
+			*n = *next;
+
+			next->next = end();
+			if (next > lastfree)
+				lastfree = next + 1;
+		}
+		else {
+			n->next = end();
+		}
+
+		count--;
 	}
+
+	node *end() {
+		return contents + length;
+	}
+
+	size_t size() { return count; }
+	bool empty() { return size() == 0; }
+	size_t capacity() { return length; }
 
 private:
 	void resize(size_t s) {
 		node *old_contents = contents;
-		size_t old_size = size;
+		size_t old_length = length;
 
-		size = s;
-		contents = new node[size];
+		length = s;
+		contents = new node[length];
 		lastfree = end();
-		for (size_t i = 0; i < size; i++) {
+		for (size_t i = 0; i < length; i++) {
 			contents[i].next = end();
 		}
 
-		for (size_t i = 0; i < old_size; i++) {
+		size_t old_count = count;
+		for (size_t i = 0; i < old_length; i++) {
 			node *old = &old_contents[i];
 			if (!is_empty(old)) {
 				(*this)[old->k] = old->v;
 			}
 		}
+		count = old_count;
 
 		delete[] old_contents;
 	}
 
 	node *main_position(const key &k) {
-		return &contents[hash()(k) % size];
+		return &contents[hash()(k) % length];
 	}
 
 	bool is_empty(node *n) {
@@ -120,16 +153,11 @@ private:
 		return end();
 	}
 
-	struct node {
-		key k;
-		value v;
-		node *next;
-	};
-
 	node *contents = nullptr;
-	size_t size = 0;
+	size_t length = 0;
 
 	node *lastfree;
+	size_t count = 0;
 };
 
 #endif
